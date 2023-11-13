58124fb1ecb3ec8728dfb0e71b3dfa9c2d1a1853
Richard Neves <richard.albuquerqueneves@gmail.com>
2023-11-13 04:26:28 -0300

    teste k3s 5

--- terraform/devAuto.tfvars
+++
@@ -1,7 +0,0 @@
-instance_type = "t2.micro"
-
-environment   = "dev"
-
-aws_profile   = "default"
-
-aws_region    = "us-east-1"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,25 @@
 variable "aws_region" {
   type        = string
   description = "The AWS region to deploy to"
-  default     = ""
+  default     = "us-east-1"
 }
 
 variable "aws_profile" {
   type        = string
   description = "The AWS profile to use to execute the commands"
-  default     = ""
+  default     = "default"
+
 }
 
 variable "environment" {
   type        = string
   description = "The environment to deploy to"
-  default     = ""
+  default     = "dev"
+
 }
 
 variable "instance_type" {
   type        = string
   description = "The instance power"
-  default     = ""
+  default     = "t2.micro"
 }
