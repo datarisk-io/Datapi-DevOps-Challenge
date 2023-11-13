b720888c4f743d9f8e4f2a76fdc0292cd4893394
Lucas Bahr <lucas.oliveira@roit.com.br>
2023-11-12 22:48:41 -0300

    feat(backend): altered envs

--- vm01/provider.tf
+++ vm01/provider.tf
@@ -10,12 +10,12 @@ terraform {
       storage_account_name = "datariskstrg"
       container_name       = "tfstatedatarisk"
       key                  = "terraform.tfstate"
-      
+      use_oidc = true
+
   }
 }
 
 
 provider "azurerm" {
-  use_oidc = true
   features {}
 }
